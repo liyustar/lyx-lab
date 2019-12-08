2505afb4c3dc1ee62fb4667c7137287109ee1e0e
liyuxing <yuxing.li@ims.jsfund.cn>
2019-12-08 17:01:44 +0800

    add lab-core lab-web

---
+++ lab-core/src/main/java/com/lyx/lab/core/bean/ResponseEntity.java
@@ -0,0 +1,9 @@
+package com.lyx.lab.core.bean;
+
+/**
+ * @Description ResponseEntity
+ * @Author liyuxing
+ * @Date 2019-12-08
+ */
+public class ResponseEntity {
+}
---
+++ lab-web/src/main/java/com/lyx/lab/web/LabWebApplication.java
@@ -0,0 +1,13 @@
+package com.lyx.lab.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabWebApplication.class, args);
+    }
+
+}
